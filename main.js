7273dd736215096c7f060002b7205af360762232
Denisson Chaves <denisson.chaves@hotmail.com>
2023-09-18 20:35:33 -0300

    feat: add function to display items in shopping list

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let listaDeItens = [];
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   salvarItem();
+  mostrarItem();
 });
 
 function salvarItem() {
@@ -27,4 +29,21 @@ function salvarItem() {
   }
 
   console.log(listaDeItens);
+}
+
+function mostrarItem() {
+  ulItens.innerHTML = "";
+  listaDeItens.forEach((item, index) => {
+    ulItens.innerHTML += `
+      <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+          <input type="checkbox" class="is-clickable" />
+          <input type="text" class="is-size-5" value="${item.valor}"></input>
+        </div>
+        <div>
+          <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+      </li>
+    `;
+  });
 }
